5a1051c2ce5cdac1334f424135a42500b0246226
bhavana162961 <125232080+bhavana162961@users.noreply.github.com>
2025-08-17 16:11:11 -0500

    Create FlightStatus.py

---
+++ FlightStatus.py
@@ -0,0 +1,16 @@
+import pandas as pd
+def getAllFlightsStatus(filename):
+    df = pd.read_excel(filename, skiprows=7, header =0)
+    df['lastupdt'] = df['lastupdt'].astype(str)
+    df['flight_dt'] = df['flight_dt'].astype(str)
+    df = df.dropna(subset=['flightkey', 'flight_dt', 'lastupdt'])
+    df['date_time'] = df[['flight_dt', 'lastupdt']].agg(' '.join, axis=1)
+    df['date_time'] = pd.to_datetime(df['date_time'], format = '%m/%d/%Y %H:%M', errors='coerce')
+    df.sort_values(by='date_time', ascending=False, inplace=True)
+    df.drop_duplicates(subset='flightkey', keep='first', inplace=True)
+    df.sort_values(by='flightnum', ascending=True, inplace=True)
+    df.drop('date_time', inplace=True, axis=1)
+    return df
+
+
+display(getAllFlightsStatus("Data Engineer_Assessment_Data Set_Flight Leg.xlsx"))
